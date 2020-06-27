b5544db912bbb945a37535773fbcd43a227b2b88
lux <john@flipsidexr.com>
2020-06-27 16:30:28 -0500

    Tweaked demo and setup to prevent default events

--- handlers/demo.php
+++ handlers/demo.php
@@ -1,23 +1,24 @@
 <?php
 
 $page->title = 'Wires Demo';
-
 $page->layout = 'admin';
-
 $this->require_admin ();
 
 Wires::init ($this);
 
 $defaults = [
 	'name' => 'Joe',
+	'age' => '27',
 	'list' => ['One', 'Two', 'Three']
 ];
 
 echo Wires::handle ($defaults, function ($res) {
-	// Process subsequent API requests
+	// Process API requests here
 	$res['list'] = ['Four', 'Five'];
+	$res['age'] = '34';
 	
-	//return Wires::error (500, 'Nope');
+	// In case of errors
+	// return Wires::error (500, 'Nope');
 
 	return $res;
 });
